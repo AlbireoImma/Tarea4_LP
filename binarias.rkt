9e0e5afc1de0a2733ae196eaaf02fffe9689aeed
AlbireoImma <faam1612@gmail.com>
2017-11-29 21:13:56 -0300

    Add files via upload

--- binarias.rkt
+++ binarias.rkt
@@ -1,4 +1,4 @@
-#lang scheme
+#lang racket
 
 (define (lenght lista)
   (if (null? lista)
@@ -14,15 +14,26 @@
     (cond ((null? lista1) ret))
     (do ((i 0 (+ i 1)))
       ((= i len) ret)
-      (if (= 1 (fun (car lista1) (car lista2)))
-          (set! ret (append ret (list 1)))
-          (set! ret (append ret (list 0)))
-          )
+      (if (= 1 (car lista1))
+          (if (= 1 (car lista2))
+              (if (fun #t #t)
+                  (set! ret (append ret (list 1)))
+                  (set! ret (append ret (list 0))))
+              (if (fun #t #f)
+                  (set! ret (append ret (list 1)))
+                  (set! ret (append ret (list 0)))))
+          (if (= 1 (car lista2))
+              (if (fun #f #t)
+                  (set! ret (append ret (list 1)))
+                  (set! ret (append ret (list 0))))
+              (if (fun #f #f)
+                  (set! ret (append ret (list 1)))
+                  (set! ret (append ret (list 0))))))
       (set! lista1 (cdr lista1))
       (set! lista2 (cdr lista2))
+          )      
     )
   )
-)
 
 
-((operar *) (list 0 1 0 1) (list 1 1 1 0))
+((operar xor) (list 0 1 0 1) (list 1 1 1 0))
